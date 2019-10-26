5ac0d89f3e95310960bd5a4d058b585711579fb0
xsummer <xsummer1997@outlook.com>
2019-10-27 01:09:07 +0800

    backup bash script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 cp -i ~/Desktop/C_prog/second_step/IO.md ~/.git/MacOS_config/
 cp -i ~/.zshrc ~/.git/MacOS_config/
 cp -i ~/.config/nvim/init.vim ~/.git/MacOS_config/
 cp -i ~/nvim_help.md ~/.git/MacOS_config/
+cp -i ~/git_use.md ~/.git/MacOS_config/
