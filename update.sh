4652e60cfe5a6bd0173cca47140f42d1f0081b37
xsummer <xsummer1997@outlook.com>
2019-10-27 13:08:03 +0800

    Git submit script

--- update.sh
+++ update.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [[ $# -lt 2 || $# -gt 2 ]];
-then
-    echo "please only input add_name and commit_content" && exit
-else
-    git add $1 && git commit -m "$2" && git push origin master
-fi
+
+git add $1 && git commit -m "$2" && git push origin master
