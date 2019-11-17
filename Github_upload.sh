ec9739233069dfa2e49b7462e5a24356f0bdd726
xsummer <xsummer1997@outlook.com>
2019-11-18 00:05:25 +0800

    Git submit script

--- Github_upload.sh
+++ Github_upload.sh
@@ -7,18 +7,18 @@ PS3="请选择要上传到GitHub的文件："
 select FILE in `ls -al . | awk '{if($2==1)print $9}'`
 do
     if [[ ! -f $COMMIT_FILE ]]; then
-        echo "请在当前目录下创建一个commit.txt文件"
+        echo "\033[33m请在当前目录下创建一个commit.txt文件\033[0m"
         exit
     else
         PS3="请选择想要提交的commit："
         select COMMIT in `cat ./commit.txt`
         do
-            git add $FILE && git commit -m $COMMIT && git push origin master
+            COMMIT_C=`echo $COMMIT | sed -n 's/_/ /gp'`
+            git add $FILE && git commit -m "${COMMIT_C}" && git push origin master
             if [[ "$?" == "0" ]]; then
-                echo -e "\033[031m上传文件成功!\033[0m"
+                echo -e "\033[032m上传文件成功!\033[0m"
                 exit
             fi
         done
     fi
-
 done
